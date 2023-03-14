53060776163fa9127dd556e4e4c2a3d60f5ce53a
Anji399 <mvpareddy3493@gmail.com>
2023-03-14 17:21:04 +0530

    malsa

--- terraform/main.tf
+++ terraform/main.tf
@@ -11,13 +11,16 @@ terraform {
     }
   }
 }
-terraform {
-  backend "s3" {
-    bucket = "mppprbuckert"
-    key    = "mpr.tfstate"
-    region = "ap-south-1"
-  }
-}
+resource "aws_s3_bucket" "terra_mpr" {
+   bucket = "terraform-mpr-devops"
+   versioning {
+      enabled = true
+   }
+} 
+resource "aws_s3_object" "alien" {
+   bucket = aws_s3_bucket.terra_mpr.id
+   key = "press.tfstate/"
+} 
 
 
 resource "aws_vpc" "main" {
