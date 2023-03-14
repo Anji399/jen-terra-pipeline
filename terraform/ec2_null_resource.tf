cade4f0bedcb149b99d7a932f872c68f5e0374ef
Anji399 <mvpareddy3493@gmail.com>
2023-03-14 21:06:28 +0530

    mihkk

--- terraform/ec2_null_resource.tf
+++ terraform/ec2_null_resource.tf
@@ -1,4 +1,4 @@
-resourse "null_resourse" "docker_deploy" {
+resource "null_resource" "docker_deploy" {
    provisioner "local-exec" {
       command = <<EOH
            echo "${aws_instance.web-1.public_ip}"
