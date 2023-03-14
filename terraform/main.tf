61c700c1c87e115ed50fa3af511a1411cfa60395
Anji399 <mvpareddy3493@gmail.com>
2023-03-14 20:37:36 +0530

    miyu

--- terraform/main.tf
+++ terraform/main.tf
@@ -13,9 +13,6 @@ terraform {
 }
 resource "aws_s3_bucket" "terra_mpr" {
    bucket = "terraform-mpr-devops"
-   versioning {
-      enabled = true
-   }
 } 
 resource "aws_s3_object" "alien" {
    bucket = aws_s3_bucket.terra_mpr.id
